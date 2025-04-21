78762223faaa83ebebd318fdeac91d0854b57fad
MertYksl03 <mert.yksl.03@gmail.com>
2025-04-21 13:41:12 +0300

    changed the touchpad script and i3 config

--- i3/scripts/toggle-touchpad.sh
+++ i3/scripts/toggle-touchpad.sh
@@ -9,9 +9,29 @@ touchpad_id=$(xinput list | grep -i touchpad | grep -o 'id=[0-9]*' | head -n1 | 
 # Get current state: enabled = 1, disabled = 0
 enabled=$(xinput list-props "$touchpad_id" | grep "Device Enabled" | awk '{print $NF}')
 
-# Toggle state
-if [ "$enabled" -eq 1 ]; then
-    xinput disable "$touchpad_id"
+# If argument is given
+if [ $# -gt 0 ]; then
+    case "$1" in
+        on)
+            xinput enable "$touchpad_id"
+            echo "Touchpad enabled"
+            ;;
+        off)
+            xinput disable "$touchpad_id"
+            echo "Touchpad disabled"
+            ;;
+        *)
+            echo "Usage: $0 [on|off]"
+            exit 1
+            ;;
+    esac
 else
-    xinput enable "$touchpad_id"
+    # Toggle if no argument
+    if [ "$enabled" -eq 1 ]; then
+        xinput disable "$touchpad_id"
+        echo "Touchpad disabled"
+    else
+        xinput enable "$touchpad_id"
+        echo "Touchpad enabled"
+    fi
 fi
